21e720165da4554a4df9f33af7a7c8785f191e8a
eapenzacharias <eapenzac@gmail.com>
2019-10-17 22:27:25 +0200

    Bug fix in User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-require 'friendship_methods'
-
 class UsersController < ApplicationController
-  include FriendshipMethods
   before_action :authenticate_user!
   def show
     @users = User.find_by(params[:user_id])
